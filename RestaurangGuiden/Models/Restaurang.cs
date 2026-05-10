a63c6a04c5accb8d9b97d70ad8f30e54b9e451da
SuhayraAhmed <suhayraahmed14@gmail.com>
2026-05-10 21:28:04 +0200

    Update Restaurang.cs

--- RestaurangGuiden/Models/Restaurang.cs
+++ RestaurangGuiden/Models/Restaurang.cs
@@ -2,9 +2,9 @@
 {
     public class Restaurang
     {
-        public int Id { get; set; } // Primär nyckel
-        public string Namn { get; set; } // Namn på restaurangen
-        public string Adress { get; set; } // Adress
+        public int Id { get; set; } 
+        public string Namn { get; set; } 
+        public string Adress { get; set; } 
         public string? BildUrl { get; set; }
         public ICollection<Meny> Menyer { get; set; } = new List<Meny>();
         public List<Omdome>? Omdomen { get; set; }
